34eed2b4d10a5fb45a348abc534000936550853c
kuratajr <n19dcat085@student.ptithcm.edu.vn>
2025-07-04 17:13:48 +0700

    update controller cron

--- cmd/dashboard/controller/cron.go
+++ cmd/dashboard/controller/cron.go
@@ -61,6 +61,7 @@ func createCron(c *gin.Context) (uint64, error) {
 	cr.Command = cf.Command
 	cr.Servers = cf.Servers
 	cr.PushSuccessful = cf.PushSuccessful
+	cr.Live = cf.Live
 	cr.NotificationGroupID = cf.NotificationGroupID
 	cr.Cover = cf.Cover
 
@@ -127,6 +128,7 @@ func updateCron(c *gin.Context) (any, error) {
 	cr.Command = cf.Command
 	cr.Servers = cf.Servers
 	cr.PushSuccessful = cf.PushSuccessful
+	cr.Live = cf.Live
 	cr.NotificationGroupID = cf.NotificationGroupID
 	cr.Cover = cf.Cover
 
